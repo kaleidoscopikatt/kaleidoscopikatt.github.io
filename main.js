9135c56dd1a7820dfafd975ece8e81a43ef1289a
envy <88539234+kaleidoscopicat@users.noreply.github.com>
2025-05-03 17:32:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const send = document.getElementById("send");
+    send.addEventListener("click", (e) => {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify({
+                parameter: "Hello, world!"
+            }),
+            mode: "cors",
+            headers: {
+                "Content-Type": "text/json",
+            },
+        }
+
+        fetch('https://script.google.com/macros/s/AKfycbzhPCk_rGQP87mFAlVO3grNVrCtdtrPmabQfs1fqA/exec', options)
+            .then(response => response.text())
+            .then(data => console.log('Success:', data))
+            .catch(error => console.error('Error:', error));
+    })
+})
